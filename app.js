ceb58897d31318b730597603daec7d4443e07201
Luciano Moreyra <luciano.moreyra774@outlook.com.ar>
2025-09-14 23:53:27 -0300

    repaso

---
+++ app.js
@@ -0,0 +1,50 @@
+// let edad = Number(22) + 10;
+// console.log(edad);
+
+// let cookies = confirm("Aceptas cookies?");
+
+// console.log(cookies);
+
+// let patria = alert("Nota IMPORTANTISIMA - NO APTO PERONCHOS");
+
+// console.log(patria);
+
+// let mensaje = prompt("Explayate wachin");
+
+
+// console.log(suma)
+
+// let resta = 10 - 5 - 2;
+
+// console.log(resta)
+
+// let division = 100 / 5;
+
+// console.log(division)
+
+// let multiplicacion = 50000000055 * 78;
+
+// console.log(multiplicacion);
+
+// let suma = 50 + 20;
+
+// if(suma == 20){
+//     console.log("La suma es 20")
+// } else if(suma < 0){
+//     console.log("La suma da un resultado negativo")
+// } else if(suma < 20){
+//     console.log("la suma no es suficiente para llegar a 20")
+// } else if(suma > 20){
+//     console.log("La suma da + de 20 puntos")
+// }
+let calificacion = 100;
+
+if(calificacion >= 100){
+    console.log("Excelente, te busca la NASA");
+} else if(calificacion >= 70){
+    console.log("Aprobado");
+} else if(calificacion >= 30){
+    console.log("Estas desaprobado, a estudiar más!!!");
+} else{
+    console.log("Tu calificación es demasiado baja, descendes de año por burro")
+}
